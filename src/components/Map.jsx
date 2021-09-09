8f8516dcb3a63d6d8dd71b2476190d3e7a15e7ed
rowlandwilliams <rowland.williams.94@gmail.com>
2021-09-09 12:38:14 +0100

    add colors and shadows

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,35 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import { caliData } from "./data/caliData";
 import { select, geoMercator, geoPath } from "d3";
 import { feature, mesh, merge } from "topojson-client";
-import randomColor from "randomcolor";
+import { generateColor, l3Codes, l3Colors } from "./utils";
 
-const l4Column = "US_L4CODE";
 const l3Column = "US_L3CODE";
 
-const l4Codes = [
-  ...new Set(
-    caliData.objects.convert.geometries.map((x) => x.properties[l4Column])
-  ),
-];
-
-const l3Codes = [
-  ...new Set(
-    caliData.objects.convert.geometries.map((x) => x.properties[l3Column])
-  ),
-];
-
-console.log(l3Codes);
-
-const colors = [];
-l4Codes.forEach((code) =>
-  colors.push({
-    code: code,
-    color: randomColor({
-      luminosity: "bright",
-      format: "rgb", // e.g. 'rgb(225,200,20)'
-    }),
-  })
-);
+const a4Width = 3508;
+const a4Height = 2480;
 
 export const Map = () => {
   const parentRef = useRef(null);
@@ -50,10 +27,12 @@ export const Map = () => {
   }, [parentRef]);
 
   useEffect(() => {
-    const l4Group = select("#map-group");
-    const outline = select("#outline");
+    const l4Group = select("#l4-group");
     const l3Group = select("#l3-group");
-    const projection = geoMercator().center([-122, 37]).scale(4500);
+    const outline = select("#outline");
+    const outline2 = select("#outline2");
+
+    const projection = geoMercator().center([-122, 41.5]).scale(10500);
 
     const pathGenerator = geoPath().projection(projection);
     const polygons = feature(caliData, caliData.objects.convert);
@@ -62,16 +41,50 @@ export const Map = () => {
       .selectAll("path")
       .data(polygons.features)
       .join("path")
-      .attr(
-        "fill",
-        (d) =>
-          colors.filter((color) => color.code === d.properties[l4Column])[0]
-            .color
-      )
-      .attr("opacity", 0.8)
+      .attr("fill", (d) => {
+        const filteredColorObj = l3Colors.filter(
+          (color) => color.code === d.properties[l3Column]
+        )[0];
+
+        return generateColor(
+          filteredColorObj.luminosityString,
+          filteredColorObj.colorString
+        );
+      })
+      .attr("fill-opacity", 0.8)
       .attr("stroke", "grey")
-      .attr("stroke-opacity", 0.4)
       .attr("d", pathGenerator);
+
+    outline2
+      .join("path")
+      .attr("stroke", "grey")
+      .attr("stroke-opacity", 0.05)
+      .attr("fill", "none")
+      .attr("stroke-width", 50)
+      .attr(
+        "d",
+        pathGenerator(
+          mesh(caliData, caliData.objects.convert, function (a, b) {
+            return a === b;
+          })
+        )
+      );
+
+    outline
+      .join("path")
+      .attr("stroke", "black")
+      .attr("stroke-opacity", 0.4)
+      .attr("filter", "url(#blur)")
+      .attr("fill", "none")
+      .attr("stroke-width", 8)
+      .attr(
+        "d",
+        pathGenerator(
+          mesh(caliData, caliData.objects.convert, function (a, b) {
+            return a === b;
+          })
+        )
+      );
 
     l3Group
       .selectAll("g")
@@ -80,10 +93,10 @@ export const Map = () => {
       .append("g")
       .attr("class", (d) => d)
       .append("path")
-      .attr("stroke", "black")
-      .attr("stroke-opacity", 0.6)
+      .attr("stroke", "#454545")
+      .attr("stroke-opacity", 0.8)
       .attr("fill", "none")
-      .attr("stroke-width", 1)
+      .attr("stroke-width", 2)
       .attr("d", (d) =>
         pathGenerator(
           merge(
@@ -94,38 +107,24 @@ export const Map = () => {
           )
         )
       );
-
-    outline
-      .join("path")
-      .attr("stroke", "black")
-      .attr("stroke-opacity", 1)
-      .attr("fill", "none")
-      .attr("stroke-width", 2)
-      .attr(
-        "d",
-        pathGenerator(
-          mesh(caliData, caliData.objects.convert, function (a, b) {
-            return a === b;
-          })
-        )
-      );
   });
 
   return (
-    <div className="w-full h-screen" ref={parentRef}>
-      <svg width={width} height="100%" id="map-svg">
-        <g
-          id="map-group"
-          transform={"translate(0," + window.innerHeight / 4 + ")"}
-        ></g>
-        <g
-          id="l3-group"
-          transform={"translate(0," + window.innerHeight / 4 + ")"}
-        ></g>
-        <path
-          id="outline"
-          transform={"translate(0," + window.innerHeight / 4 + ")"}
-        ></path>
+    <div className="w-full h-screen m-4" ref={parentRef}>
+      <svg width={a4Width} height={a4Height} id="map-svg">
+        <defs>
+          <filter id="blur">
+            <feGaussianBlur stdDeviation="5"></feGaussianBlur>
+          </filter>
+        </defs>
+        <rect width="100%" height="100%" fill="white" stroke="black"></rect>
+        <path id="outline2"></path>
+        <path id="outline"></path>
+
+        <g id="l4-group">
+          {/* <rect width="100%" height="100%" fill="grey"></rect> */}
+        </g>
+        <g id="l3-group"></g>
       </svg>
     </div>
   );
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,1 +1,93 @@
+import randomColor from "randomcolor";
 
+export const possibleShades = ["light", "bright"];
+export const generateColor = (luminosityString, colorString) => {
+  return randomColor({
+    luminosity: luminosityString,
+    hue: colorString,
+  });
+};
+
+export const l3Codes = [
+  "1",
+  "9",
+  "6",
+  "7",
+  "8",
+  "85",
+  "5",
+  "4",
+  "13",
+  "78",
+  "80",
+  "14",
+  "81",
+];
+
+export const l3Colors = [
+  {
+    code: "1",
+    colorString: "blue",
+    luminosityString: "bright",
+  },
+  {
+    code: "9",
+    colorString: "orange",
+    luminosityString: "bright",
+  },
+  {
+    code: "6",
+    colorString: "yellow",
+    luminosityString: "dark",
+  },
+  {
+    code: "7",
+    colorString: "orange",
+    luminosityString: "bright",
+  },
+  {
+    code: "8",
+    colorString: "green",
+    luminosityString: "light",
+  },
+  {
+    code: "85",
+    colorString: "orange",
+    luminosity: "bright",
+  },
+  {
+    code: "5",
+    colorString: "green",
+    luminosityString: "bright",
+  },
+  {
+    code: "4",
+    colorString: "blue",
+    luminosityString: "light",
+  },
+  {
+    code: "13",
+    colorString: "pink",
+    luminosity: "light",
+  },
+  {
+    code: "78",
+    colorString: "green",
+    luminosityString: "light",
+  },
+  {
+    code: "80",
+    colorString: "yellow",
+    luminosityString: "light",
+  },
+  {
+    code: "14",
+    colorString: "red",
+    luminosityString: "light",
+  },
+  {
+    code: "81",
+    colorString: "red",
+    luminosityString: "light",
+  },
+];
